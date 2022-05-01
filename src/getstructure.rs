1a649160451bc901879f2d5964018c76e05c67ab
merelymyself <poonsters6@gmail.com>
2022-05-01 19:22:55 +0800

    fmt

--- src/getstructure.rs
+++ src/getstructure.rs
@@ -1,7 +1,7 @@
 use rand::Rng;
 
-fn gen_adjectives(adjectives:i32) -> Vec<char> {
-    let adj_array = ['O','S','A','C','M'];
+fn gen_adjectives(adjectives: i32) -> Vec<char> {
+    let adj_array = ['O', 'S', 'A', 'C', 'M'];
     let mut adj_array2 = Vec::new();
     let mut rng = rand::thread_rng();
     for _ in 0..adjectives {
@@ -23,8 +23,8 @@ fn gen_adjectives(adjectives:i32) -> Vec<char> {
     return adj_array2;
 }
 
-fn get_starting_structure1(adjectives:i32, plural:bool) -> Vec<char> {
-    let mut struct_array:Vec<char> = Vec::new();
+fn get_starting_structure1(adjectives: i32, plural: bool) -> Vec<char> {
+    let mut struct_array: Vec<char> = Vec::new();
     let mut rng = rand::thread_rng();
     if plural == false || rng.gen_bool(0.5) {
         struct_array.push('0');
@@ -39,43 +39,46 @@ fn get_starting_structure1(adjectives:i32, plural:bool) -> Vec<char> {
     return struct_array;
 }
 
-fn get_ending_structure1(adverbs:i32, plural:bool, ex_struct:Vec<char>) -> Vec<char> {
+fn get_ending_structure1(adverbs: i32, plural: bool, ex_struct: Vec<char>) -> Vec<char> {
     let mut struct_array = ex_struct;
     if plural == true {
         struct_array.push('v');
-    }
-    else {
+    } else {
         struct_array.push('V');
     }
-    for _ in 0..adverbs{
+    for _ in 0..adverbs {
         struct_array.push('a');
     }
     struct_array.push('x');
     return struct_array;
 }
 
-fn get_ending_structure2(adverbs:i32, plural:bool, ex_struct:Vec<char>) -> Vec<char> {
+fn get_ending_structure2(adverbs: i32, plural: bool, ex_struct: Vec<char>) -> Vec<char> {
     let mut struct_array = ex_struct;
-    for _ in 0..adverbs{
+    for _ in 0..adverbs {
         struct_array.push('a');
     }
     if plural == true {
         struct_array.push('v');
-    }
-    else {
+    } else {
         struct_array.push('V');
     }
     struct_array.push('x');
     return struct_array;
 }
 
-fn get_ending_structure3(adverbs:i32, plural:bool, adjectives2:i32, plural2:bool, ex_struct:Vec<char>) -> Vec<char> {
+fn get_ending_structure3(
+    adverbs: i32,
+    plural: bool,
+    adjectives2: i32,
+    plural2: bool,
+    ex_struct: Vec<char>,
+) -> Vec<char> {
     let mut rng = rand::thread_rng();
     let mut struct_array = ex_struct;
     if plural == true {
         struct_array.push('v');
-    }
-    else {
+    } else {
         struct_array.push('V');
     }
     for _ in 0..adverbs {
@@ -94,7 +97,13 @@ fn get_ending_structure3(adverbs:i32, plural:bool, adjectives2:i32, plural2:bool
     return struct_array;
 }
 
-fn get_ending_structure4(adverbs:i32, plural:bool, adjectives2:i32, plural2:bool, ex_struct:Vec<char>) -> Vec<char> {
+fn get_ending_structure4(
+    adverbs: i32,
+    plural: bool,
+    adjectives2: i32,
+    plural2: bool,
+    ex_struct: Vec<char>,
+) -> Vec<char> {
     let mut rng = rand::thread_rng();
     let mut struct_array = ex_struct;
     for _ in 0..adverbs {
@@ -102,8 +111,7 @@ fn get_ending_structure4(adverbs:i32, plural:bool, adjectives2:i32, plural2:bool
     }
     if plural == true {
         struct_array.push('v');
-    }
-    else {
+    } else {
         struct_array.push('V');
     }
     if plural2 == false || rng.gen_bool(0.5) {
@@ -119,16 +127,35 @@ fn get_ending_structure4(adverbs:i32, plural:bool, adjectives2:i32, plural2:bool
     return struct_array;
 }
 
-pub fn get_structure(adjectives:i32, adverbs:i32, structure:i32, plural:bool, adjectives2:i32, plural2:bool) -> Vec<char>{
+pub fn get_structure(
+    adjectives: i32,
+    adverbs: i32,
+    structure: i32,
+    plural: bool,
+    adjectives2: i32,
+    plural2: bool,
+) -> Vec<char> {
     return if structure == 1 {
         get_ending_structure1(adverbs, plural, get_starting_structure1(adjectives, plural))
     } else if structure == 2 {
         get_ending_structure2(adverbs, plural, get_starting_structure1(adjectives, plural))
     } else if structure == 3 {
-        get_ending_structure3(adverbs, plural, adjectives2,plural2, get_starting_structure1(adjectives, plural))
+        get_ending_structure3(
+            adverbs,
+            plural,
+            adjectives2,
+            plural2,
+            get_starting_structure1(adjectives, plural),
+        )
     } else if structure == 4 {
-        get_ending_structure4(adverbs, plural, adjectives2,plural2, get_starting_structure1(adjectives, plural))
+        get_ending_structure4(
+            adverbs,
+            plural,
+            adjectives2,
+            plural2,
+            get_starting_structure1(adjectives, plural),
+        )
     } else {
         Vec::from(['x'])
-    }
+    };
 }
--- src/getword.rs
+++ src/getword.rs
@@ -17,12 +17,12 @@ fn get_line_at(path: &Path, line_num: usize) -> Result<String, Error> {
 }
 */
 
-fn count_lines_string(str:&String) -> i32 {
+fn count_lines_string(str: &String) -> i32 {
     let lines = str.lines();
     return lines.count() as i32;
 }
 
-fn get_word_no(count:i32, str:&String) -> String {
+fn get_word_no(count: i32, str: &String) -> String {
     let mut count2 = count - 1;
     let mut lines = str.lines();
     while count2 != 0 {
@@ -49,9 +49,20 @@ a - adverbs
 V - verb, except for the singular form - aka 'coders code' vs 'coder codes': this is the latter
  */
 
-pub fn get_word(firstletter:char) -> String {
+pub fn get_word(firstletter: char) -> String {
     let orderforfunction: [char; 10] = ['a', 'A', 'C', 'v', 'M', 'n', 'O', 'p', 's', 'S'];
-    let words = [String::from_utf8(include_bytes!("adverbs.txt").to_vec()).expect("error"), String::from_utf8(include_bytes!("ageadjectives.txt").to_vec()).expect("error"), String::from_utf8(include_bytes!("colouradjectives.txt").to_vec()).expect("error"), String::from_utf8(include_bytes!("verbs.txt").to_vec()).expect("error"), String::from_utf8(include_bytes!("materialadjectives.txt").to_vec()).expect("error"), String::from_utf8(include_bytes!("nouns.txt").to_vec()).expect("error"), String::from_utf8(include_bytes!("opinionadjectives.txt").to_vec()).expect("error"), String::from_utf8(include_bytes!("pluralnouns.txt").to_vec()).expect("error"), String::from_utf8(include_bytes!("singularnouns.txt").to_vec()).expect("error"), String::from_utf8(include_bytes!("sizeadjectives.txt").to_vec()).expect("error")];
+    let words = [
+        String::from_utf8(include_bytes!("adverbs.txt").to_vec()).expect("error"),
+        String::from_utf8(include_bytes!("ageadjectives.txt").to_vec()).expect("error"),
+        String::from_utf8(include_bytes!("colouradjectives.txt").to_vec()).expect("error"),
+        String::from_utf8(include_bytes!("verbs.txt").to_vec()).expect("error"),
+        String::from_utf8(include_bytes!("materialadjectives.txt").to_vec()).expect("error"),
+        String::from_utf8(include_bytes!("nouns.txt").to_vec()).expect("error"),
+        String::from_utf8(include_bytes!("opinionadjectives.txt").to_vec()).expect("error"),
+        String::from_utf8(include_bytes!("pluralnouns.txt").to_vec()).expect("error"),
+        String::from_utf8(include_bytes!("singularnouns.txt").to_vec()).expect("error"),
+        String::from_utf8(include_bytes!("sizeadjectives.txt").to_vec()).expect("error"),
+    ];
     let mut wordtype: usize = 0;
     return if firstletter == '0' {
         String::from("the")
@@ -64,33 +75,42 @@ pub fn get_word(firstletter:char) -> String {
         let mut rng = rand::thread_rng();
         let outputnumber = rng.gen_range(1..(count_lines_string(&words[3]) + 1));
         let mut word = get_word_no(outputnumber, &words[3]);
-        if &word[word.len() - 2..word.len()] == &String::from("sh") || word.chars().last().unwrap() == 'x' || word.chars().last().unwrap() == 's' {
+        if &word[word.len() - 2..word.len()] == &String::from("sh")
+            || word.chars().last().unwrap() == 'x'
+            || word.chars().last().unwrap() == 's'
+        {
             word.push('e');
         }
         // rush -> rushes. Previously, rush -> rushs. Similar for box -> boxes. Toss -> tosses.
         word.push('s');
         word
-    } else if firstletter == 'p'{
+    } else if firstletter == 'p' {
         let mut rng = rand::thread_rng();
-        if rng.gen_bool((count_lines_string(&words[7])) as f64/(((count_lines_string(&words[7])) as f64)+((count_lines_string(&words[5])) as f64))){
+        if rng.gen_bool(
+            (count_lines_string(&words[7])) as f64
+                / (((count_lines_string(&words[7])) as f64)
+                    + ((count_lines_string(&words[5])) as f64)),
+        ) {
             let outputnumber = rng.gen_range(1..(count_lines_string(&words[7]) + 1));
             let word = get_word_no(outputnumber, &words[7]);
             word
-        }
-        else {
+        } else {
             let outputnumber = rng.gen_range(1..(count_lines_string(&words[5]) + 1));
             let mut word = get_word_no(outputnumber, &words[5]);
             word.push('s');
             word
         }
-    } else if firstletter == 's'{
+    } else if firstletter == 's' {
         let mut rng = rand::thread_rng();
-        if rng.gen_bool((count_lines_string(&words[8])) as f64/(((count_lines_string(&words[8])) as f64)+((count_lines_string(&words[5])) as f64))){
+        if rng.gen_bool(
+            (count_lines_string(&words[8])) as f64
+                / (((count_lines_string(&words[8])) as f64)
+                    + ((count_lines_string(&words[5])) as f64)),
+        ) {
             let outputnumber = rng.gen_range(1..(count_lines_string(&words[8]) + 1));
             let word = get_word_no(outputnumber, &words[8]);
             word
-        }
-        else {
+        } else {
             let outputnumber = rng.gen_range(1..(count_lines_string(&words[5]) + 1));
             let word = get_word_no(outputnumber, &words[5]);
             word
@@ -104,5 +124,5 @@ pub fn get_word(firstletter:char) -> String {
         let mut rng = rand::thread_rng();
         let outputnumber = rng.gen_range(1..(count_lines_string(&words[wordtype]) + 1));
         get_word_no(outputnumber, &words[wordtype])
-    }
+    };
 }
